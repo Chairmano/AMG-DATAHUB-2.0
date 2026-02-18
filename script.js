8da8d149eb063ef8ccc75d7190c127e8f135767a
Tha Genius <126257898+Chairmano@users.noreply.github.com>
2026-02-18 21:32:33 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.classList.toggle("translate-x-full");
+}
+
+function toggleTheme() {
+  document.body.classList.toggle("bg-white");
+  document.body.classList.toggle("text-black");
+}
